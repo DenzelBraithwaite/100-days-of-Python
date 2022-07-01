bfea01ca39e437c82d3df3a45900828913255c84
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-06-30 21:15:09 -0400

    adding first 2 days of work for Python 100 day challenge

---
+++ day_one/band_name_generator.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Sup dude, how's it hanging? You need a new band name?")
+#2. Ask the user for the city that they grew up in.
+city = input("Sounds like a plan, what city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("Cool thanks, now give me a pet name.\n")
+#4. Combine the name of their city and pet and show them their band name.
+print(f'Your super duper band name is: {city} {pet}!!!')
+#5. Make sure the input cursor shows on a new line, see the example at:
