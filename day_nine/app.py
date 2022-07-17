1a6a8ca6e2fd65794532ddb59681118ce3b41dd5
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-07-17 14:42:23 -0400

    init day 9 project

---
+++ day_nine/app.py
@@ -0,0 +1,7 @@
+import os
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+
+print(logo)
+# the os.system clear depends on your terminal. Windows uses 'cls' and linux uses 'clear'
+os.system('clear')
